937e5bd758f4277783a40ebd0665cd7d39813e4f
gabmorelli <gabriele.morelli@sixt.net>
2012-02-11 10:15:58 +0100

    first commit

---
+++ src/com/gmorelli/svegliatube/SvegliaTubeActivity.java
@@ -0,0 +1,13 @@
+package com.gmorelli.svegliatube;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SvegliaTubeActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
